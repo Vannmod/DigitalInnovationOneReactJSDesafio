609f1e60c800babc3616ea5a20dcbbe3ef353ddf
Vannmod <rafaelbattglin94@gmail.com>
2022-06-11 21:08:00 -0300

    Inclusão de novos arquivos e modificações

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import { Card, Cardcontent} from './Card'
+import { Button } from './Button'
+import { Grid } from './Grid'
+import { Typography } from './Typography'
+import { Skeleton } from './Skeleton'
+import { Select } from './Select'
+import { MenuItem } from './MenuItem'
+
+export {
+  Button,
+  Card,
+  Cardcontent,
+  Grid,
+  Typography,
+  Skeleton,
+  Select,
+  MenuItem
+}
